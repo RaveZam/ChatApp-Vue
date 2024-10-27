8c2d6c7227a4d015aeb83b321f70dba72506d1ba
Duke <114975455+RaveZam@users.noreply.github.com>
2024-10-27 13:30:32 +0800

    Updated Libraries with Icons using Iconify. Added Tailwinds Successfully

--- chatapp-vue/src/App.vue
+++ chatapp-vue/src/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
+import MessageIcon from "./components/MessageIcon.vue";
 </script>
 
 <template>
+  <Header />
   <div class="bg-blue-200">
+    <h1>hello</h1>
     <HelloWorld msg="Vite + Vue" />
+    <MessageIcon />
   </div>
 </template>
---
+++ chatapp-vue/src/components/Header.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import MessageIcon from "./MessageIcon.vue";
+import { defineComponent } from "vue";
+
+defineComponent({
+  name: "Header",
+});
+</script>
+
+<template>
+  <div class="bg-gray-50 h-16 flex items-center">
+    <MessageIcon class="scale-150 ml-4 text-black" />
+    <h1 class="p-4">Message App</h1>
+  </div>
+</template>
