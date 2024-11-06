1726aa5a76e4b5cbc5b9d72d5c61dd263c20722d
Duke <114975455+RaveZam@users.noreply.github.com>
2024-11-06 11:06:37 +0800

    Setting up chat area

--- chatapp-vue/src/App.vue
+++ chatapp-vue/src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <Header />
-  <Sidebar />
+  <div>
+    <Header />
+    <div class="flex">
+      <Sidebar />
+      <ChatArea />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import Header from "./components/Header.vue";
 import Sidebar from "./components/Sidebar.vue";
+import ChatArea from "./components/chat/ChatArea.vue";
 </script>
---
+++ chatapp-vue/src/components/chat/ChatArea.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-full pt-16">
+    <h1 class="p-4 text-[2vw]">Message Area</h1>
+    <div class="p-4 pr-8 mx-4 rounded-2xl text-right">
+      <h2 class="mr-4">{{ username }}</h2>
+      <div class="bg-blue-400 p-2 rounded-2xl">
+        <span class="text-gray-700">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel numquam
+          sapiente harum perspiciatis deleniti facilis repellat? Eaque,
+          laboriosam quidem architecto nobis ullam dolorum itaque, eveniet
+          deleniti qui, sed iure quibusdam.</span
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const username = ref("Runielle Raven" as string);
+</script>
