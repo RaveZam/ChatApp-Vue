d288c99f707fcc9fd73409a77bed0349869a1eb9
Duke <114975455+RaveZam@users.noreply.github.com>
2024-11-07 11:03:10 +0800

    Adding pusher

--- chatapp-vue/src/components/chat/ChatArea.vue
+++ chatapp-vue/src/components/chat/ChatArea.vue
@@ -1,21 +1,71 @@
 <template>
-  <div class="w-full pt-16">
-    <h1 class="p-4 text-[2vw]">Message Area</h1>
+  <div class="w-full pt-16 justify-between flex flex-col">
+    <div class="flex justify-between">
+      <h1 class="p-4 text-[2vw]">Message Area</h1>
+      <input
+        class="mr-12 h-12 p-4 border-2 border-gray-800 rounded-md"
+        type="text"
+        placeholder="input username"
+        v-model="username"
+      />
+    </div>
     <div class="p-4 pr-8 mx-4 rounded-2xl text-right">
-      <h2 class="mr-4">{{ username }}</h2>
-      <div class="bg-blue-400 p-2 rounded-2xl">
-        <span class="text-gray-700">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel numquam
-          sapiente harum perspiciatis deleniti facilis repellat? Eaque,
-          laboriosam quidem architecto nobis ullam dolorum itaque, eveniet
-          deleniti qui, sed iure quibusdam.</span
-        >
+      <div
+        class="bg-blue-500 p-4 rounded-2xl"
+        v-for="message in messages"
+        :key="messages"
+      >
+        <h2 class="mr-4">{{ message.username }}</h2>
+        <span class="text-white">{{ message.message }}</span>
       </div>
+    </div>
+
+    <div class="mb-8">
+      <form @submit.prevent="submit" class="flex mr-4">
+        <input
+          class="flex-1 border-2 border-gray-600 rounded-md p-4"
+          type="text"
+          placeholder="input message"
+          v-model="message"
+        />
+        <button
+          type="submit"
+          class="bg-blue-500 text-white mx-4 p-4 px-8 rounded-full"
+        >
+          Send
+        </button>
+      </form>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-const username = ref("Runielle Raven" as string);
+import Pusher from "pusher-js";
+import { ref, onMounted } from "vue";
+
+interface ChatMessage {
+  username: string;
+  message: string;
+}
+
+const username = ref("Runielle Raven");
+const messages = ref<ChatMessage[]>([]);
+const message = ref("");
+
+onMounted(() => {
+  Pusher.logToConsole = true;
+
+  const pusher = new Pusher("8f26045999c3fc72d5a0", {
+    cluster: "ap1",
+  });
+
+  const channel = pusher.subscribe("my-channel");
+  channel.bind("my-event", (data: ChatMessage) => {
+    messages.value.push(data);
+  });
+});
+
+const submit = () => {
+  console.log("pushed");
+};
 </script>
